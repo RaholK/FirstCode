6135002e07be2ec2fd10fd7d4bba9ed0cc70adac
RaholK <rahol97@live.de>
2015-04-29 13:38:07 +0200

    Create HalloWelt.java
    Beispiel aus dem Unterricht

---
+++ HalloWelt.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+
+public class showmessage {
+	public static void main (String args[]) {
+		JOptionPane.showMessageDialog(null,
+            "Sie müssen eine Ganzzahl eingeben",
+            "Eine Nachricht",					      
+            	JOptionPane.WARNING_MESSAGE);
+	}
+
+}
